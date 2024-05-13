a12bbd6f16fb5e6d4044eca12eac7933bcbc415b
kanimozhijv <64355561+kanimozhijv@users.noreply.github.com>
2024-05-13 14:05:56 +0530

    Create Source.cpp

---
+++ Assignment_Two/Source/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"StringCalculator.h"
+
+using namespace std;
+int main()
+{
+    StringCalculator* obj;
+    int result = obj->add("Calculator");
+    cout << "Hello from Sample App"<< result;
+}
